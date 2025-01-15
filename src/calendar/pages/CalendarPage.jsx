bbd354488a08327c838622260bc4a781ae60503e
Valentina Maldonado <poiowtf@192.168.1.118>
2025-01-15 11:46:42 -0500

    add helpers

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,11 +1,54 @@
+import { Calendar } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { addHours } from 'date-fns'
+
 import { Navbar } from "../"
+import { localizer, getMessagesES } from '../../helpers';
+
+const events = [{
+  title: 'Cumpleaños del Poio',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#c98feb',
+  user: {
+    _id: '123',
+    name: 'Laura'
+  }
+}]
 
 export const CalendarPage = () => {
-  return (
-   <>
-   <Navbar />
 
-   
-   </>
+ const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log(event, start, end, isSelected)
+
+    const style = {
+      backgroundColor: 'red',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+ }
+
+  return (
+    <>
+      <Navbar />
+
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px )' }}
+        messages={getMessagesES()}
+        eventPropGetter={ eventStyleGetter }
+      />
+    </>
   )
 }
